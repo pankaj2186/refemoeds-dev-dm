cc1a94b37820d4eb62f8701d236b9e456a30e653
pankaj2186 <pansingh@adobe.com>
2025-12-10 19:07:46 +0530

    Update dynamic-media-video.js

--- blocks/dynamic-media-video/dynamic-media-video.js
+++ blocks/dynamic-media-video/dynamic-media-video.js
@@ -1,1 +1,9 @@
+import { getDynamicMediaServerURL } from '../../scripts/utils.js';
 
+
+/**
+ * @param {HTMLElement} $block
+ */
+export default async function decorate(block) {
+  console.log("dynamic-media-video :"+block);
+}
