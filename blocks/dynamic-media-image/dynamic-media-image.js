fea613fcfab7bdeaef9a2512dfb4612bd547d43a
pankaj2186 <pansingh@adobe.com>
2025-12-10 11:23:55 +0530

    Create dynamic-media-image.js

---
+++ blocks/dynamic-media-image/dynamic-media-image.js
@@ -0,0 +1,9 @@
+import { getDynamicMediaServerURL } from '../../scripts/utils.js';
+
+
+/**
+ * @param {HTMLElement} $block
+ */
+export default async function decorate(block) {
+  console.log("dynamic-media-image :"+block);
+}
