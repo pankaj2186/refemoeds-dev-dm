d60d4f7385bb0bb861ba44eba80a573fc4cc6e04
pankaj2186 <pansingh@adobe.com>
2026-01-13 10:36:39 +0530

    if image not authored, then hiding everything else

--- blocks/dynamic-media-image/dynamic-media-image.js
+++ blocks/dynamic-media-image/dynamic-media-image.js
@@ -1,9 +1,15 @@
-import { getDynamicMediaServerURL } from '../../scripts/utils.js';
-
-
 /**
- * @param {HTMLElement} $block
+ * @param {HTMLElement} block
  */
-export default async function decorate(block) {
-  console.log("dynamic-media-image :"+block);
+export default function decorate(block) {
+  //console.log("dynamic-media-image :"+block);
+
+  const links = block.querySelectorAll('a[href]');
+  // If no links exist, hide everything else within the block
+  if (links.length === 0) {
+    // Hide all children of the block
+    Array.from(block.children).forEach((child) => {
+      child.style.display = 'none';
+    });
+  }
 }
